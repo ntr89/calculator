5f3ad50aa9e2df320a1021f7129ef5ec55ac2810
Nick P <npotapov2@gmail.com>
2020-07-18 20:38:24 -0400

    splitting functions some keyboard inputs working

--- source/js/calculator.js
+++ source/js/calculator.js
@@ -6,38 +6,39 @@ var mySwitch = false;
 var myOpe = ["+", "-", "*", "/"];
 //console.log(mybtn);
 for (var i = 0; i < mybtn.length; i++) {
-  mybtn[i].addEventListener("click", function () {
-    var myValue = this.innerHTML;
-    if (myCom || myCal == "0") {
-      myCom = false;
-      myCal = "";
-    }
-    if (myValue == "+" || myValue == "-" || myValue == "*" || myValue == "/") {
-      if (mySwitch) {
-        mySwitch = false;
-        if (myOpe.indexOf(myOutput.innerHTML.slice(-1)) > -1) {
-          myCal = myCal.substring(0, myCal.length - 1);
-        } else {
-          myCal = eval(myCal);
-        }
-      }
-      mySwitch = true;
-    }
-    if (myValue == "=") {
-      myValue = "";
-      if (myOpe.indexOf(myOutput.innerHTML.slice(-1)) == -1) {
+  mybtn[i].addEventListener("click", doTheCalc);
+}
+function doTheCalc() {
+  var myValue = this.innerHTML;
+  if (myCom || myCal == "0") {
+    myCom = false;
+    myCal = "";
+  }
+  if (myValue == "+" || myValue == "-" || myValue == "*" || myValue == "/") {
+    if (mySwitch) {
+      mySwitch = false;
+      if (myOpe.indexOf(myOutput.innerHTML.slice(-1)) > -1) {
+        myCal = myCal.substring(0, myCal.length - 1);
+      } else {
         myCal = eval(myCal);
       }
-    } else if (myValue == "C") {
-      myCal = 0;
-      myCom = true;
-    } else if (myOutput.innerHTML.indexOf(".") > -1 && myValue == ".") {
-      myValue = "";
-    } else {
-      myCal = myCal + myValue;
     }
-    myOutput.innerHTML = myCal;
-  });
+    mySwitch = true;
+  }
+  if (myValue == "=") {
+    myValue = "";
+    if (myOpe.indexOf(myOutput.innerHTML.slice(-1)) == -1) {
+      myCal = eval(myCal);
+    }
+  } else if (myValue == "C") {
+    myCal = 0;
+    myCom = true;
+  } else if (myOutput.innerHTML.indexOf(".") > -1 && myValue == ".") {
+    myValue = "";
+  } else {
+    myCal = myCal + myValue;
+  }
+  myOutput.innerHTML = myCal;
 }
 jQuery(document).keypress(function (e) {
   var key = e.keyCode;
